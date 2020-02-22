98384d05c0f9b7df6f1bc044f039a8c5d3c031d0
Sarah Murray <sarahmurraydev@gmail.com>
2020-02-21 23:23:47 -0500

    clean up some intial code not being used and styles

--- billboard-ui/src/App.js
+++ billboard-ui/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Chart from './Chart'
@@ -7,21 +6,8 @@ import Chart from './Chart'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Chart />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Get Billboard Data</h1>
+      <Chart />
     </div>
   );
 }
