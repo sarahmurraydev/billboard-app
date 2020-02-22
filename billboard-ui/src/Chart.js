c96bc1e7b753184bfdd0c820f91dd39045c66c0f
Sarah Murray <sarahmurraydev@gmail.com>
2020-02-21 23:40:41 -0500

    take in parameters form the ui on what chart and date to return

--- billboard-ui/src/Chart.js
+++ billboard-ui/src/Chart.js
@@ -6,11 +6,29 @@ const API_URL = 'http://localhost:5000';
 class Chart extends React.Component {
 
     state = {
-        chart: null
+        chart: null,
+        date: null, 
+        type: null
     }
 
-    getChartData = () => {
-        axios.get(`${API_URL}/give-me-charts/2019-07-20`)
+    handleInputChange = (inputName) => event => {
+        event.preventDefault()
+
+        let newState = {};
+        newState[inputName] = event.target.value;
+        this.setState(newState)
+    }
+
+    onClick = () => event => {
+        event.preventDefault()
+        let chartType = this.state.type;
+        let chartDate = this.state.date;
+
+        this.getChartData(chartType, chartDate);
+    }
+
+    getChartData = (type, date) => {
+        axios.get(`${API_URL}/give-me-charts/${type}/${date}`)
             .then(response => {
                 console.log("got a response!", response)
                 this.setState({
@@ -25,14 +43,17 @@ class Chart extends React.Component {
     render(){
         let chartData = this.state.chart
         return <div style={{width: '75%'}} className="chart-data">
-            <button onClick={this.getChartData}>Click here to get the data!</button>
+            <input type='input' onChange={this.handleInputChange('type')}></input>
+            <input type='input' onChange={this.handleInputChange('date')}></input>
+            <button onClick={this.onClick()}>Click here to get the data!</button>
             {!!chartData && <div>
                 <h3>Now Viewing the Billboard {chartData.name.toUpperCase()} for the week of {chartData.date}</h3>
                 <ol>
                     {chartData.entries.map((entry, i) => {
                         return <li key={`${chartData.date}-#${i}`}>{entry.artist} - {entry.title}</li>
                     })}
-            </ol>}
+                </ol>
+            </div>}
         </div>
     }
 }
